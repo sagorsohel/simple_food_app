d29b83eb22b11587ceeb26183758ea57cf59ed8f
sagorsohel <sohel19sagor@gmail.com>
2024-03-15 01:41:15 +0600

    add bg color

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_food_app/style/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Food App'),
-      ),
+      backgroundColor: AppColors.backgroundColor,
       body: Container(),
     );
   }
---
+++ lib/style/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xffFFCA28);
+  static const backgroundColor = Color(0xff0f172a);
+}
