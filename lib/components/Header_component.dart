c505babf34cb373652e274e78563e7e0c2aa0d3c
sagorsohel <sohel19sagor@gmail.com>
2024-03-15 02:10:46 +0600

    add header component

---
+++ lib/components/Header_component.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:simple_food_app/style/app_colors.dart';
+
+class HeaderComponent extends StatelessWidget {
+  const HeaderComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: AppColors.lightBackgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ),
+          const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Deliver To',
+                style: TextStyle(color: AppColors.primaryColor, fontSize: 25),
+              ),
+              Text(
+                'Sohel,Rajshahi',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage(
+                      'https://lh3.googleusercontent.com/ogw/AF2bZyg6aflM_I4b4FTogenFtqqBvWYHAW1IleAQdk0bFQ=s32-c-mo')),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_food_app/components/Header_component.dart';
 import 'package:simple_food_app/style/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,53 +7,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: AppColors.lightBackgroundColor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Icon(
-                      Icons.menu,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Deliver To',
-                        style: TextStyle(
-                            color: AppColors.primaryColor, fontSize: 25),
-                      ),
-                      Text(
-                        'Sohel,Rajshahi',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              'https://lh3.googleusercontent.com/ogw/AF2bZyg6aflM_I4b4FTogenFtqqBvWYHAW1IleAQdk0bFQ=s32-c-mo')),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            HeaderComponent(),
           ],
         ),
       ),
