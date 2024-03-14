27d030dc25f859e884378b4e29f36cd3207b4553
sagorsohel <sohel19sagor@gmail.com>
2024-03-15 01:59:46 +0600

    add image

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,37 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundColor,
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: AppColors.lightBackgroundColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://lh3.googleusercontent.com/ogw/AF2bZyg6aflM_I4b4FTogenFtqqBvWYHAW1IleAQdk0bFQ=s32-c-mo')),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/style/app_colors.dart
+++ lib/style/app_colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primaryColor = Color(0xffFFCA28);
-  static const backgroundColor = Color(0xff0f172a);
+  static const backgroundColor = Color(0xff0e1420);
+  static const lightBackgroundColor = Color(0xff30363d);
 }
